76a1ec2535e72d455ba2573575d26e6a39e58154
amandaxm <amandaxaviermariano10@gmail.com>
2020-09-02 11:01:43 -0300

    Injetando um serviço em outro serviço

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CursosModule
   ],
  // providers: [],
-  providers: [CursosService],
+  providers: [CursosService,LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -2,6 +2,9 @@
 //injeção de dependencia para isso 
 
 import { Injectable, EventEmitter } from '@angular/core';
+import { LogService } from '../shared/log.service';
+
+
 //avisa ao angular que é uma classe injetavel
 @Injectable()
 export class CursosService{
@@ -12,13 +15,16 @@ export class CursosService{
     static criouNovoCurso = new EventEmitter<string>();
 
     private cursos: string[]=['Angular2','Java','Node'];
-    constructor(){
+    //instancia log service disponivel
+    constructor(private logService: LogService){
         console.log('CursoService');
     }
     getCursos(){
+        this.logService.consolelog('Obtendo lista de curso');
        return this.cursos; 
     }
     addCurso(curso: string){
+        this.logService.consolelog(`Criando novo curso ${curso}`);
         this.cursos.push(curso);
         //emitir toda vez que um curso for criado
         this.emitirCursoCriado.emit(curso);
